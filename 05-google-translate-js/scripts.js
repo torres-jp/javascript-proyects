521c2884d427598011f26289e10cbe7e3e55ce52
torres-jp <torres.jpw@gmail.com>
2025-08-13 17:29:36 -0300

    add fix

--- 05-google-translate-js/scripts.js
+++ 05-google-translate-js/scripts.js
@@ -1,9 +1,9 @@
-import { $ } from './doom.js'
+import { $, $$ } from './doom.js'
 
 class GoogleTranslator {
   static SUPPORTED_LANGUAGES = ['en', 'es', 'fr', 'de', 'it', 'pt', 'ru', 'ja']
 
-  static FULL_LANGUAGES_CODE = {
+  static FULL_LANGUAGES_CODES = {
     en: 'en-US',
     es: 'es-ES',
     fr: 'fr-FR',
@@ -17,24 +17,22 @@ class GoogleTranslator {
   static DEFAULT_SOURCE_LANGUAGE = 'es'
   static DEFAULT_TARGET_LANGUAGE = 'en'
 
-  consturctor() {
+  constructor() {
     this.init()
     this.setupEventListeners()
 
-    this.translationTimeout = null
-    this.currentTranslator = null
     this.currentTranslator = null
     this.currentDetector = null
   }
 
   init() {
-    // recuperamos todos los elementos del DOM
+    // Recuperamos todos los elementos del DOM que necesitamos
     this.inputText = $('#inputText')
     this.outputText = $('#outputText')
 
     this.sourceLanguage = $('#sourceLanguage')
     this.targetLanguage = $('#targetLanguage')
-    this.swapLanguages = $('#swapLanguages')
+    this.swapLanguage = $('#swapLanguages')
 
     this.micButton = $('#micButton')
     this.copyButton = $('#copyButton')
@@ -43,7 +41,7 @@ class GoogleTranslator {
     // configuracion inicial
     this.targetLanguage.value = GoogleTranslator.DEFAULT_TARGET_LANGUAGE
 
-    // verificar que el usuario tenga soporte para la API
+    // Verificar el soporte para la API
     this.checkAPISupport()
   }
 
@@ -52,92 +50,34 @@ class GoogleTranslator {
     this.hasNativeDetector = 'LanguageDetector' in window
 
     if (!this.hasNativeTranslator || !this.hasNativeDetector) {
-      console.warm(
-        'APIs Nativas de traduccion y deteccion de idiomas no soportadas en tu navegador'
+      console.warrm(
+        'APIS nativas de traduccion y deteccion no soportadas en tu navegador'
       )
-      //this.showWarning()
+      // thi.showWarning()
     } else {
-      console.log('Apis Nativas disponibles')
+      console.log('Apis Nativas de IA Disponibles')
     }
   }
 
   setupEventListeners() {
     this.inputText.addEventListener('input', () => {
-      // actualizar contador de letras
-      // traducir texto con un debounce
-      this.debounceTranslate()
+      // Actualizar el contador de letras
+      // Traducir el texto con un debounce
     })
 
     this.sourceLanguage.addEventListener('change', () => this.translate())
     this.targetLanguage.addEventListener('change', () => this.translate())
 
-    this.swapLanguages.addEventListener('click', () => this.swapLanguages())
+    this.swapLanguage.addEventListener('click', () => this.swapLanguage())
   }
 
-  debounceTranslate() {
-    clearTimeout(this.translationTimeout)
-    this.translationTimeout = setTimeout(() => {
-      this.translate()
-    }, 500)
-  }
-
-  async getTranslation(text) {
-    const sourceLanguage = this.sourceLanguage.value
-    const targetLanguage = this.targetLanguage.value
-
-    if (sourceLanguage === targetLanguage) return text
-
-    // verificar si tenemos disponibilidad entre lenguajes
-    try {
-      const status = await window.Translator.availability({
-        sourceLanguage,
-        targetLanguage,
-      })
-
-      if (status === 'unavailable') {
-        throw new Error(
-          `Traduccion no disponible entre ${sourceLanguage} y ${targetLanguage}`
-        )
-      }
-    } catch (error) {
-      console.log(error)
-    }
-
-    // realizar la traduccion
-    const translatorKey = `${sourceLanguage}-${targetLanguage}`
-    if (
-      !this.currentTranslator ||
-      !this.currentTranslatorKey !== translatorKey
-    ) {
-      this.currentTranslator = await window.Translator.create({
-        sourceLanguage,
-        targetLanguage,
-      })
-    }
-  }
-
-  async translate() {
-    const text = this.inputText.value.trim()
-    if (!text) {
-      this.outputText.textContent = ''
-      return
-    }
-
-    this.outputText.textContent = 'Traduciendo....'
-
-    try {
-      const translation = await this.getTranslation(text)
-      this.outputText.textContent = translation
-      // Aqui se llama a la API de traduccion
-    } catch (error) {
-      console.log(error)
-      this.outputText.textContent = 'Error al traducir'
-    }
-  }
-
-  swapLanguages() {
+  translate() {
     // TODO!
+  }
+
+  swapLanguage() {
+    // intercambiar idiomas entre el sourceLanguage y el targetlanguage
   }
 }
 
-const GoogleTranslator = new GoogleTranslator()
+const googleTranslator = new GoogleTranslator()
